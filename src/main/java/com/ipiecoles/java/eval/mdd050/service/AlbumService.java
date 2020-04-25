637f6c69531f62b564f28deb09b2e103bbe9d5a1
FRC <fabrice.rene-corail@hotmail.fr>
2020-04-25 20:47:45 +0200

    feat album service implementation

--- src/main/java/com/ipiecoles/java/eval/mdd050/service/AlbumService.java
+++ src/main/java/com/ipiecoles/java/eval/mdd050/service/AlbumService.java
@@ -2,7 +2,13 @@ package com.ipiecoles.java.eval.mdd050.service;
 
 import com.ipiecoles.java.eval.mdd050.model.Album;
 import com.ipiecoles.java.eval.mdd050.repository.AlbumRepository;
+
+import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,14 +22,23 @@ public class AlbumService {
         return albumRepository.save(album);
     }
 
-    public void deleteAlbum(Long id) {
+    public ResponseEntity<String> deleteAlbum(Long id) {
         albumRepository.deleteById(id);
+        return ResponseEntity.ok("album deleted");
     }
 
 	public Album save(Album album) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+    public Album findById(Long id) {
+        java.util.Optional<Album> artist = this.albumRepository.findById(id);
+        if(!artist.isPresent()){
+            throw new EntityNotFoundException("Impossible de trouver l'artiste d'identifiant " + id);
+        }
+        return artist.get();
+    }
 
 	
 }
